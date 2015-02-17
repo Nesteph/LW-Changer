bc4511409bba5333a715ce76118056e1ab639bb2
Steve McMillin <steve.mcmillin@gmail.com>
2015-02-16 19:01:10 -0600

    Updated logic
    Added a plist for storing settings and supporting config modification
    from the command line/MDM.
    Added logic to read in settings from plist.
    Added better logic for keeping the backup folder clean.

--- Library/LWChanger/LoginWindowDownload.sh
+++ Library/LWChanger/LoginWindowDownload.sh
@@ -1,12 +1,39 @@
 #!/bin/sh
 
-# Change to the url or local file you want to use
-# Make sure to comment out the format not being used.
+# Read in settings from config
+imageURL=`defaults read /Library/Preferences/LWChanger.plist imageURL`
+NumToBackup=`defaults read /Library/Preferences/LWChanger.plist NumToBackup`
+BackupLocation=`defaults read /Library/Preferences/LWChanger.plist BackupLocation`
+FrequencyCheck=`defaults read /Library/Preferences/LWChanger.plist FrequencyCheck`
 
-# Local file syntax
-# imageURL="file:///path/to/local/image.png"
+# Update LaunchDaemon to match config settings
+# launchd modification here
 
-# Web based syntax
-imageURL="https://mypuzzlingpiece.files.wordpress.com/2014/07/images-background-rainbow-spectrum.jpg"
+# Download the latest image if the Modify date is newer than our current login window background.
+curl -z /Library/Caches/com.apple.desktop.admin.png $imageURL > /Library/LWChanger/com.apple.desktop.admin.png
 
-curl -z /Library/Caches/com.apple.desktop.admin.png $imageURL > /Library/Caches/com.apple.desktop.admin.png
+# Check to see if the updated image has been downloaded, if so, move it to 
+if [ -e "/Library/LWChanger/com.apple.desktop.admin.png" ]; then
+  
+  # Check to see if any backups need to be kept
+  if [ $NumToBackup != 0 ]; then
+    
+    # Make loop counter for number of backup images to keep
+    Num=echo "$((NumToBackup-1))"
+
+    # Loop through existing backups and move them for newer incoming image
+    for (($Num; $Num>0; Num--)); do
+
+      # Checks to see if a backup already exists, and if it does, moves it for the new one
+      if [ -e "$BackupLocation/com.apple.desktop.admin.png.$Num"]; then
+        next=$BackupLocation/com.apple.desktop.admin.png.`echo "$(($Num+1))"`
+        mv "$BackupLocation/com.apple.desktop.admin.png.$Num" $next
+      fi
+    done
+    # Backup the current image to the backup location
+    mv /Library/Caches/com.apple.desktop.admin.png "$BackupLocation/com.apple.desktop.admin.png.1"
+  fi
+  
+  # Move the image downloaded earlier to be used for the login window background
+  mv /Library/LWChanger/com.apple.desktop.admin.png /Library/Caches/com.apple.desktop.admin.png
+fi
