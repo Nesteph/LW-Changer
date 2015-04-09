453895167fda0341e2e1882f64156e53ae37e2db
Nesteph <nesteph@users.noreply.github.com>
2015-04-09 18:54:54 -0500

    Resets the login wallpaper back to factory default

--- Library/LWChanger/Uninstall.sh
+++ Library/LWChanger/Uninstall.sh
@@ -5,3 +5,4 @@ launchctl unload /Library/LaunchDaemons/LWChanger.plist
 rm /Library/LaunchDaemons/LWChanger.plist
 rm /Library/Preferences/LWChanger.plist
 rm -rf /Library/LWChanger
+rm /Library/Caches/com.apple.desktop.admin.png
